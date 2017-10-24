52d03012fbb21b9c119de33a9ff521952a168722
Florian Knop <florian_knop@hotmail.com>
2017-10-24 13:39:55 +0200

    feat: initial models

--- forum/models.py
+++ forum/models.py
@@ -2,5 +2,44 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Thread(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=255)
+    private = models.BooleanField(default=True)
+
+    skills = models.ManyToManyField("skills.Skill", blank=True)
+
+    def messages(self):
+        return Message.objects.filter(thread=self).order_by("created_date")
+
+
+class Message(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    thread = models.ForeignKey(Thread)
+    parent_message = models.ForeignKey("self", null=True)
+
+    content = models.TextField()
+
+    # attachments = models.ManyToManyField() TODO
+
+    def replies(self):
+        replies = []
+        for message in Message.objects.filter(parent_message=self):
+            replies.append(message)
+
+        return replies
+
+    def all_replies(self):
+        replies = {self: []}
+        for message in Message.objects.filter(parent_message=self).order_by("created_date"):
+            replies[self].append(message.all_replies())
+
+        return replies
--- forum/tests.py
+++ forum/tests.py
@@ -1,6 +1,105 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Thread, Message
+
+
+class ThreadModelTest(TestCase):
+    def testCreateThread(self):
+        user = User()
+        user.save()
+        thread = Thread(title="Help on Calculus", author=user)
+        thread.save()
+
+        self.assertEquals(thread.title, "Help on Calculus")
+
+        thread.title = "Help"
+        thread.save()
+
+        self.assertEquals(thread.title, "Help")
+
+    def testMessages(self):
+        user = User()
+        user.save()
+
+        thread = Thread(title="test", author=user)
+        thread.save()
+
+        first_message = Message(thread=thread, content="hello")
+        first_message.save()
+
+        second_message = Message(thread=thread, content="hello as well")
+        second_message.save()
+
+        messages = thread.messages()
+
+        self.assertEquals(messages[0].id, first_message.id)
+        self.assertEquals(messages[1].id, second_message.id)
+
+    def testReplies(self):
+        user = User()
+        user.save()
+
+        thread = Thread(title="test", author=user)
+        thread.save()
+
+        first_message = Message(thread=thread, content="hello")
+        first_message.save()
+
+        second_message = Message(thread=thread, content="hello as well", parent_message=first_message)
+        second_message.save()
+
+        messages = thread.messages()
+
+        self.assertEquals(messages[0].id, first_message.id)
+        self.assertEquals(messages[1].id, second_message.id)
+
+        replies = first_message.replies()
+        self.assertEquals(replies[0], second_message)
+
+        all_replies = first_message.all_replies()
+        self.assertEquals(all_replies[first_message], [{second_message: []}])
+
+
+    def testAllReplies(self):
+        user = User()
+        user.save()
+
+        thread = Thread(title="test", author=user)
+        thread.save()
+
+        first_message = Message(thread=thread, content="hello")
+        first_message.save()
+
+        second_message = Message(thread=thread, content="hello as well", parent_message=first_message)
+        second_message.save()
+
+        third_message = Message(thread=thread, content="test", parent_message=first_message)
+        third_message.save()
+
+        fourth_message = Message(thread=thread, content="test", parent_message=second_message)
+        fourth_message.save()
+
+        fifth_message = Message(thread=thread, content="test", parent_message=fourth_message)
+        fifth_message.save()
+
+        all_replies = first_message.all_replies()
+        self.assertEquals(all_replies, {
+            first_message: [
+                {
+                    second_message: [
+                        {
+                            fourth_message: [
+                                {fifth_message: []}
+                            ]
+                        }
+                    ]
+                },
+                {
+                    third_message: []
+                }
+            ]
+        })
