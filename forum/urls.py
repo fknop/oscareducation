1e543ccb5251682697c637a328a8f17d6d8a126c
Florian Knop <florian_knop@hotmail.com>
2017-10-26 21:30:06 +0200

    feat: updates to initial urls and views

--- forum/urls.py
+++ forum/urls.py
@@ -7,10 +7,8 @@ from . import views
 
 urlpatterns = [
     url(r'^forum/', views.forum_dashboard, name='forum_dashboard'),
-    url(r'^forum/write', views.write_thread, name='forum_write'),
-    url(r'^forum/write/create', views.create_thread, name ='forum_create_thread'),
-    url(r'^forum/thread/(?P<id>\d+)', views.view_thread, name='view_thread'),
-    url(r'^forum/thread/(?P<id>\d+)/reply', views.reply_thread, name='reply_thread')
+    url(r'^forum/write', views.create_thread, name='forum_write'),
+    url(r'^forum/thread/(?P<id>\d+)', views.thread, name='view_thread'),
 ]
 
 
--- forum/views.py
+++ forum/views.py
@@ -1,35 +1,81 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django import forms
+from django.db import transaction
 from django.shortcuts import render, get_object_or_404
 from django.core.exceptions import PermissionDenied
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
-from django.views.decorators.http import require_POST
-
+from django.http import HttpResponseRedirect, HttpResponse
+from django.views.decorators.http import require_POST, require_GET
 
 # Create your views here.
+from forum.models import Thread, Message
+from skills.models import Skill
 
+
+class ThreadForm(forms.Form):
+    title = forms.TextInput()
+    skills = forms.MultipleChoiceField(queryset=Skill.objects.all())
+    content = forms.Textarea()
+    visibility = forms.ChoiceField()
+
+
+@require_GET
 def forum_dashboard(request):
     pass
 
-def write_thread(request):
-    pass
 
-
-@require_POST
 def create_thread(request):
-    pass
-
-
-def view_thread(request, id):
-    pass
-
-
-@require_POST
-def reply_thread(request, id):
-
     """
+    GET: return the page to create a thread
+    POST: create a thread
+    """
+    if request.method == 'GET':
+        return get_create_thread_page(request)
+
+    if request.method == 'POST':
+        return post_create_thread(request)
+
+
+def get_create_thread_page(request):
+    pass
+
+
+def post_create_thread(request):
+    form = ThreadForm(request.POST)
+
+    if form.is_valid():
+        title = form.cleaned_data['title']
+        skills = form.cleaned_data['skills']
+        content = form.cleaned_data['content']
+
+        with transaction.atomic():
+            thread = Thread(title=title, skills=skills)
+            # TODO: set visibility
+            thread.save()
+            original_message = Message(content=content, parent_thread=thread)
+            original_message.save()
+
+    pass
+
+
+def thread(request, id):
+    """
+    GET method: return the page of a thread
+    POST method: reply to a thread
+    """
+    if request.method == 'GET':
+        return get_thread(request, id)
+
+    if request.method == 'POST':
+        return reply_thread(request, id)
+
+
+def get_thread(request, id):
+    pass
+
+
+def reply_thread(request, id):
     message_id = request.GET.get('message_id')
 
     content = ""  # TODO: access content
@@ -41,6 +87,3 @@ def reply_thread(request, id):
         message.parent_message = parent_message
 
     return HttpResponse()
-    """
-
-    pass
