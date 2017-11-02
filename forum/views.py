761df536962b4f59c6291cd6bc1a5ee0864a0906
Paul_Kriwin <paul.kriwin@student.uclouvain.be>
2017-11-02 17:39:23 +0100

    new thread backend completed (lacks validation)

--- forum/views.py
+++ forum/views.py
@@ -3,18 +3,16 @@ from __future__ import unicode_literals
 
 from django import forms
 from django.contrib.auth.models import User
-from django.shortcuts import render, get_object_or_404
+from django.db import transaction
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.views.decorators.http import require_POST, require_GET
 
 # Create your views here.
 from forum.models import Thread, Message
-
-
-class ThreadForm(forms.Form):
-    title = forms.TextInput()
-    content = forms.Textarea()
-    visibility = forms.ChoiceField()
+from promotions.models import Lesson
+from skills.models import Skill
+from users.models import Professor
 
 
 @require_GET
@@ -37,27 +35,49 @@ def create_thread(request):
 def get_create_thread_page(request):
     return render(request, "forum/new_thread.haml")
 
-
+class ThreadForm(forms.Form):
+    title = forms.CharField()
+    visibdata = forms.CharField()
+    skills = forms.CharField()
+    content = forms.CharField()
 
 def post_create_thread(request):
 
-    """
     form = ThreadForm(request.POST)
 
     if form.is_valid():
+
         title = form.cleaned_data['title']
-        skills = form.cleaned_data['skills']
+        visibility = request.POST.get('visibility')
+        visibdata = form.cleaned_data['visibdata']
+        skills = form.cleaned_data['skills'].encode('utf8').split(" ")
         content = form.cleaned_data['content']
+        author = User.objects.get(pk=request.user.id)
 
         with transaction.atomic():
-            thread = Thread(title=title, skills=skills)
-            # TODO: set visibility
-            thread.save()
-            original_message = Message(content=content, parent_thread=thread)
-            original_message.save()
-    """
 
-    return HttpResponse()
+            thread = Thread(title=title, author=author)
+
+            if visibility == 'private':
+                thread.recipient = User.objects.get(pk=visibdata)
+
+            if visibility == 'class':
+                thread.lesson = Lesson.objects.get(pk=visibdata)
+
+            if visibility == 'public':
+                thread.professor = Professor.objects.get(pk=visibdata)
+
+            thread.save()
+            thread.skills = Skill.objects.filter(pk__in=skills)
+            thread.save()
+
+            original_message = Message(content=content, thread=thread, author=author)
+            original_message.save()
+
+        return redirect('/forum/thread/' + str(thread.id))
+
+    else:
+        return redirect('/forum/write')
 
 
 def thread(request, id):
