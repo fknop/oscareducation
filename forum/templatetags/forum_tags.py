85ddb6c6a8a8a4fe9af634ece31d3d1db5dee5cf
Florian Knop <florian_knop@hotmail.com>
2017-11-04 21:27:30 +0100

    feat: message anchors

--- forum/templatetags/forum_tags.py
+++ forum/templatetags/forum_tags.py
@@ -2,12 +2,18 @@ from django.template.loader_tags import register
 
 
 @register.inclusion_tag('forum/message.haml')
-def message_partial(message, user, level=0):
+def message_partial(message, user, reply_to, level=0):
     return {
         "message": message,
+        "reply_to": reply_to,
         "level": level + 1,
         "user": user
     }
+
+
+@register.inclusion_tag('forum/reply_form.haml')
+def reply_form_partial():
+    return {}
 
 
 @register.filter
@@ -18,3 +24,8 @@ def reply_margin(level):
 @register.filter
 def can_edit(user, message):
     return message.author == user
+
+@register.filter
+def is_reply_to(reply_to, message):
+    print(reply_to, str(message.id))
+    return str(message.id) == reply_to
