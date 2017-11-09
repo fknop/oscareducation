bb83c70b4cb6ff9ec5de200d23b8e33e2e618ca9
N dV <n.devaucleroy@gmail.com>
2017-11-09 08:19:43 +0100

    add first selenium test: admin login. Requirements updated

--- forum/tests.py
+++ forum/tests.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import time
+
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.test import TestCase, Client, RequestFactory
@@ -14,6 +16,10 @@ from .views import deepValidateAndFetch
 from dashboard import private_threads, public_class_threads, public_teacher_threads_student, get_thread_set
 from views import create_thread, reply_thread
 
+from django.test import LiveServerTestCase
+from selenium_intro.selenium_tests.test import SeleniumTestCase
+from selenium_intro.selenium_tests.webdriver import CustomWebDriver
+from django.core.urlresolvers import reverse
 
 class ThreadModelTest(TestCase):
     def test_invalid_thread_both_recipient_professor(self):
@@ -497,3 +503,57 @@ class TestGetWritePage(TestCase):
     def test_get_write_page(self):
         response = self.c.get('/forum/write/')
         self.assertEquals(response.status_code, 200)
+
+class SeleniumTestCase(LiveServerTestCase):
+    """
+    A base test case for selenium, providing hepler methods for generating
+    clients and logging in profiles.
+    """
+        
+    def open(self, url):
+        self.wd.get("%s%s" % (self.live_server_url, url))
+
+class Auth(SeleniumTestCase):
+
+    def setUp(self):
+        # setUp is where you setup call fixture creation scripts
+        # and instantiate the WebDriver, which in turns loads up the browser.
+        User.objects.create_superuser(username='admin',
+                                      password='pw',
+                                      email='info@lincolnloop.com')
+
+        # Instantiating the WebDriver will load your browser
+        self.wd = CustomWebDriver()
+
+    def tearDown(self):
+        # Don't forget to call quit on your webdriver, so that
+        # the browser is closed after the tests are ran
+        self.wd.quit()
+
+    # Just like Django tests, any method that is a Selenium test should
+    # start with the "test_" prefix.
+    def test_login(self):
+        """
+        Django Admin login test
+        """
+        # Open the admin index page
+        self.open(reverse('admin:index'))
+
+        # Selenium knows it has to wait for page loads (except for AJAX requests)
+        # so we don't need to do anything about that, and can just
+        # call find_css. Since we can chain methods, we can
+        # call the built-in send_keys method right away to change the
+        # value of the field
+        time.sleep(20)
+        self.wd.find_css('#id_username').send_keys("admin")
+        # for the password, we can now just call find_css since we know the page
+        # has been rendered
+        self.wd.find_css("#id_password").send_keys('pw')
+        # You're not limited to CSS selectors only, check
+        # http://seleniumhq.org/docs/03_webdriver.html for
+        # a more compreehensive documentation.
+        self.wd.find_element_by_xpath('//input[@value="Connexion"]').click()
+        # Again, after submiting the form, we'll use the find_css helper
+        # method and pass as a CSS selector, an id that will only exist
+        # on the index page and not the login page
+        self.wd.find_css("#content-main")
